429b4cafecb8e58ea13c8794bf0f697082f20ab6
mujahid <m.alsouti0@gmail.com>
2026-04-22 09:34:03 +0400

    Replace repeated separator literal with constant

--- bikeshare_2.py
+++ bikeshare_2.py
@@ -5,6 +5,8 @@ CITY_DATA = { 'chicago': 'chicago.csv',
               'new york city': 'new_york_city.csv',
               'washington': 'washington.csv' }
 
+SEPARATOR = '-' * 40
+
 def get_filters():
     """
     Asks user to specify a city, month, and day to analyze.
@@ -24,7 +26,7 @@ def get_filters():
     # get user input for day of week (all, monday, tuesday, ... sunday)
 
 
-    print('-'*40)
+    print(SEPARATOR)
     return city, month, day
 
 
@@ -60,7 +62,7 @@ def time_stats(df):
 
 
     print("\nThis took %s seconds." % (time.time() - start_time))
-    print('-'*40)
+    print(SEPARATOR)
 
 
 def station_stats(df):
@@ -79,7 +81,7 @@ def station_stats(df):
 
 
     print("\nThis took %s seconds." % (time.time() - start_time))
-    print('-'*40)
+    print(SEPARATOR)
 
 
 def trip_duration_stats(df):
@@ -95,7 +97,7 @@ def trip_duration_stats(df):
 
 
     print("\nThis took %s seconds." % (time.time() - start_time))
-    print('-'*40)
+    print(SEPARATOR)
 
 
 def user_stats(df):
@@ -114,7 +116,7 @@ def user_stats(df):
 
 
     print("\nThis took %s seconds." % (time.time() - start_time))
-    print('-'*40)
+    print(SEPARATOR)
 
 
 def main():
